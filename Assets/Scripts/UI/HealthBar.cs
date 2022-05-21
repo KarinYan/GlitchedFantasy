47a3a12a125b0d9245e960dbfab4d21abc4d7ff6
KarinA <karineakulyan@gmail.com>
2022-05-21 13:43:21 +0200

    Modificación nomenclatura scripts

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,44 +1,84 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using static Platformer.Core.Simulation;
 
-//Clase encargada de mostrar el sprite del estado de salud del jugador
-public class HealthBar : MonoBehaviour
+namespace Platformer.Mechanics
 {
-    public Sprite emptyHeart, halfHeart, fullHeart;
-    Image heartImage;
-
-    //Función que inicializa el sprite de la barra de estado al cargarse la escena
-    private void Awake()
+    //Clase que actualiza la barra del estado de salud
+    public class HealthBar : MonoBehaviour
     {
-        heartImage = GetComponent<Image>();
-    }
+        public GameObject heartPrefab;
+        public PlayerManager playerHealth;
+        List<StatusHearts> hearts = new List<StatusHearts>();
 
-    //Función que muestra el sprite de corazones correspondiente al estado de salud del jugador
-    public void SetHeartImage(HeartStatus status)
-    {
-        switch (status)
+        //Función que se ejecuta siempre que el objeto se activa, para suscribirse a los eventos del PlayerManager 
+        private void OnEnable()
         {
-            case HeartStatus.Empty:
-                heartImage.sprite = emptyHeart;
-                break;
-            case HeartStatus.Half:
-                heartImage.sprite = halfHeart;
-                break;
-            case HeartStatus.Full:
-                heartImage.sprite = fullHeart;
-                break;
+            PlayerManager.OnPlayerDamaged += DrawHearts;
+            PlayerManager.OnPlayerHealed += DrawHearts;
+        }
+        private void OnDisable()
+        {
+            PlayerManager.OnPlayerDamaged -= DrawHearts;
+            PlayerManager.OnPlayerHealed -= DrawHearts;
+        }
+
+        //Función que ejecuta la actualización de la barra de estado siempre que el objeto se activa
+        private void Start()
+        {
+            DrawHearts();
+        }
+        
+        //Función que actualiza la barra de estado en varios pasos:
+        //1. Limpiar la barra de estado
+        //2. Crear corazones vacíos hasta el límite de la salud máxima definida en PlayerHealth
+        //3. Actualizar la imagen de los corazones según el estado actual del jugador definido en PlayerHealth
+        public void DrawHearts()
+        {
+            ClearHearts();
+
+            float maxHealthRemainder = playerHealth.maxHealth % 2;
+            
+            int heartsToMake = (int)(playerHealth.maxHealth / 2 + maxHealthRemainder);
+            for(int i = 0; i < heartsToMake; i++)
+            {
+                CreateEmptyHeart();
+            }
+            
+            for(int i = 0; i < hearts.Count; i++)
+            {
+                int heartStatusRemainder = (int)Mathf.Clamp(playerHealth.health - (i * 2), 0, 2);
+                hearts[i].SetHeartImage((HeartStatus)heartStatusRemainder);
+            }
+
+            foreach(Transform t in transform)
+            {
+                t.GetComponent<RectTransform>().localScale = Vector3.one;
+            }
+        }
+
+        //Función que crea un corazón vacío en la barra de estado
+        public void CreateEmptyHeart()
+        {
+            GameObject newHeart = Instantiate(heartPrefab);
+        
+            newHeart.transform.SetParent(transform);
+            StatusHearts heartComponent = newHeart.GetComponent<StatusHearts>();
+            heartComponent.SetHeartImage(HeartStatus.Empty);
+            hearts.Add(heartComponent);
+        }
+
+        //Función que limpia la barra de estado destruyendo todos los corazones y creando una lista vacía
+        public void ClearHearts()
+        {
+            foreach(Transform t in transform)
+            {               
+                Destroy(t.gameObject);
+            }
+            hearts = new List<StatusHearts>();
         }
     }
 }
-
-//Numeración que identifica el estado de salud del jugador
-public enum HeartStatus
-{
-    Empty = 0,
-    Half = 1,
-    Full = 2
-}
-
---
+++ Assets/Scripts/UI/StatusHearts.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static Platformer.Core.Simulation;
+
+//Clase encargada de mostrar el sprite del estado de salud del jugador
+public class StatusHearts : MonoBehaviour
+{
+    public Sprite emptyHeart, halfHeart, fullHeart;
+    Image heartImage;
+
+    //Función que inicializa el sprite de la barra de estado al cargarse la escena
+    private void Awake()
+    {
+        heartImage = GetComponent<Image>();
+    }
+
+    //Función que muestra el sprite de corazones correspondiente al estado de salud del jugador
+    public void SetHeartImage(HeartStatus status)
+    {
+        switch (status)
+        {
+            case HeartStatus.Empty:
+                heartImage.sprite = emptyHeart;
+                break;
+            case HeartStatus.Half:
+                heartImage.sprite = halfHeart;
+                break;
+            case HeartStatus.Full:
+                heartImage.sprite = fullHeart;
+                break;
+        }
+    }
+}
+
+//Numeración que identifica el estado de salud del jugador
+public enum HeartStatus
+{
+    Empty = 0,
+    Half = 1,
+    Full = 2
+}
+
